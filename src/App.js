a3d175fef132d9418216f7ab3dc176d9782f9dc9
Afsaneh <a.habibi1394@gmail.com>
2020-05-31 02:15:33 +0430

    Add routing and layout schema

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
 import './App.css';
-import {Demo} from './component/formDisplay.js';
+import { FormClass } from './component/formDisplay.js';
+import { MainLayout } from './component/mainPage.js';
+import { LoadMoreList } from './component/formsList.js';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+const { Header, Content, Footer } = Layout;
 function App() {
   return (
-    <div className="App">
-      <Demo/>
-    </div>
+    <Router>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+          <Switch>
+            <Route exact path="/" >
+              <LoadMoreList />
+            </Route>
+            <Route path='/form/:handle' component={FormClass}>
+              {/* <FormClass /> */}
+            </Route>
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}></Footer>
+      </Layout>
+      {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul> */}
+
+      <hr />
+
+      {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+    </Router>
   );
 }
 
